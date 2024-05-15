372234d369301255f1863af8c6711841bacf5354
Nabin Kawan <nabin.kawan.9@gmail.com>
2024-05-15 11:59:15 +0545

    test: Metadata anchor in vote transaction

--- tests/govtool-frontend/playwright/lib/pages/governanceActionDetailsPage.ts
+++ tests/govtool-frontend/playwright/lib/pages/governanceActionDetailsPage.ts
@@ -1,5 +1,7 @@
 import environments from "@constants/environments";
-import { Page } from "@playwright/test";
+import { downloadMetadata } from "@helpers/metadata";
+import { Download, Page } from "@playwright/test";
+import metadataBucketService from "@services/metadataBucketService";
 
 export default class GovernanceActionDetailsPage {
   readonly voteBtn = this.page.getByTestId("vote-button");
@@ -23,6 +25,7 @@ export default class GovernanceActionDetailsPage {
     "view-other-details-button"
   );
   readonly continueModalBtn = this.page.getByTestId("continue-modal-button");
+  readonly confirmModalBtn = this.page.getByTestId("confirm-modal-button");
 
   readonly voteSuccessModal = this.page.getByTestId("alert-success");
   readonly externalLinkModal = this.page.getByTestId("external-link-modal");
@@ -42,9 +45,36 @@ export default class GovernanceActionDetailsPage {
     );
   }
 
-  async vote() {
+  async vote(context?: string) {
     await this.yesVoteRadio.click();
+
+    if (context) {
+      await this.contextBtn.click();
+      await this.contextInput.fill(context);
+      await this.confirmModalBtn.click();
+      await this.page.getByRole("checkbox").click();
+      await this.confirmModalBtn.click();
+
+      this.page
+        .getByRole("button", { name: "download Vote_Context.jsonld" })
+        .click();
+      const voteMetadata = await this.downloadVoteMetadata();
+      const url = await metadataBucketService.uploadMetadata(
+        voteMetadata.name,
+        voteMetadata.data
+      );
+
+      await this.page.getByPlaceholder("URL").fill(url);
+      await this.confirmModalBtn.click();
+      await this.page.getByTestId("go-to-vote-modal-button").click();
+    }
+
     await this.voteBtn.click();
+  }
+
+  async downloadVoteMetadata() {
+    const download: Download = await this.page.waitForEvent("download");
+    return downloadMetadata(download);
   }
 
   async reVote() {
