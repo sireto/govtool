965e7575b5cbbf9f2fb403b103bfe1aff69b4d89
Nabin Kawan <nabin.kawan.9@gmail.com>
2024-05-16 22:18:22 +0545

    test: Search & Filter dRep

--- tests/govtool-frontend/playwright/tests/2-delegation/delegation.spec.ts
+++ tests/govtool-frontend/playwright/tests/2-delegation/delegation.spec.ts
@@ -1,16 +1,52 @@
-import { setAllureEpic } from "@helpers/allure";
-import { expect, test } from "@playwright/test";
+import {dRep01Wallet} from "@constants/staticWallets";
+import DRepDirectoryPage, {dRepFilterOptions} from "@pages/dRepDirectoryPage";
+import {setAllureEpic} from "@helpers/allure";
+import {expect, test} from "@playwright/test";
 
 test.beforeEach(async () => {
-  await setAllureEpic("2. Delegation");
+    await setAllureEpic("2. Delegation");
 });
+
 test("2C. Verify DRep Behavior in Disconnected State", async ({ page }) => {
   await page.goto("/");
 
-  await page.getByTestId("delegate-connect-wallet-button").click();
+  await page.getByTestId("view-drep-directory-button").click();
   await page
     .locator('[data-testid$="-connect-to-delegate-button"]')
     .first()
     .click();
   await expect(page.getByTestId("connect-your-wallet-modal")).toBeVisible();
+});
+
+test("2J. Should search by DRep id", async ({ page }) => {
+  const dRepDirectory = new DRepDirectoryPage(page);
+  await dRepDirectory.goto();
+
+  await dRepDirectory.searchInput.fill(dRep01Wallet.dRepId);
+  await expect(dRepDirectory.getDRepCard(dRep01Wallet.dRepId)).toHaveText(
+    dRep01Wallet.dRepId
+  );
+});
+
+test("2K. Should filter DReps", async ({ page }) => {
+  const dRepDirectory = new DRepDirectoryPage(page);
+  await dRepDirectory.goto();
+
+  await dRepDirectory.filterBtn.click();
+
+  // Single filter
+  for (const option of dRepFilterOptions) {
+    await dRepDirectory.filterDRepByNames([option]);
+    await dRepDirectory.validateFilters([option]);
+    await dRepDirectory.unFilterDRepByNames([option]);
+  }
+
+  // Multiple filters
+  const multipleFilterOptionNames = [...dRepFilterOptions];
+  while (multipleFilterOptionNames.length > 1) {
+    await dRepDirectory.filterDRepByNames(multipleFilterOptionNames);
+    await dRepDirectory.validateFilters(multipleFilterOptionNames);
+    await dRepDirectory.unFilterDRepByNames(multipleFilterOptionNames);
+    multipleFilterOptionNames.pop();
+  }
 });
