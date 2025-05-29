263e3db0f76c91a761200becb4f04b376dae01bb
Niraj <duwalkneerose@gmail.com>
2025-05-29 22:03:37 +0545

    fix: increase timeout for verify identity button

--- tests/govtool-frontend/playwright/lib/helpers/auth.ts
+++ tests/govtool-frontend/playwright/lib/helpers/auth.ts
@@ -57,7 +57,7 @@ export async function createAuthWithUserName({
 
   const proposalDiscussionPage = new ProposalDiscussionPage(page);
   await proposalDiscussionPage.goto();
-  await proposalDiscussionPage.verifyIdentityBtn.click({ timeout: 15_000 });
+  await proposalDiscussionPage.verifyIdentityBtn.click({ timeout: 60_000 });
 
   await proposalDiscussionPage.setUsername(mockValid.username());
 
